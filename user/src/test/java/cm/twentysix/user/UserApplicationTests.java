91c3d73829b841d86a22d66c4e5fcb168053a847
haden <80521474+thdefn@users.noreply.github.com>
2024-08-08 10:31:17 +0900

    chore: creating user module

---
+++ user/src/test/java/cm/twentysix/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.twentysix.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

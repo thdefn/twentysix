d3d8b1dd719494f0d822f0bbf0104a95a8f4c18d
haden <80521474+thdefn@users.noreply.github.com>
2024-08-10 18:00:10 +0900

    chore: creating a brand module used by seller

---
+++ brand/src/test/java/cm/twentysix/brand/BrandApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.twentysix.brand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

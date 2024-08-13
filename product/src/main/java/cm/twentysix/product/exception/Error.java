a1555bb7ddaa9f3a092f47e738f260031686188d
haden <80521474+thdefn@users.noreply.github.com>
2024-08-13 16:04:03 +0900

    feat: communication between the product server and the brand server -  Implementation of GRPC communication between the product server and the brand server - Add brand information to product registration and update feature

--- product/src/main/java/cm/twentysix/product/exception/Error.java
+++ product/src/main/java/cm/twentysix/product/exception/Error.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum Error {
+    NOT_BRAND_ADMIN(HttpStatus.NOT_FOUND, "브랜드 관리자가 아닙니다."),
+    GRPC_COMMUNICATION_ERROR(HttpStatus.BAD_REQUEST, "grpc 통신 중 문제가 발생했습니다."),
     FILE_UPLOAD_FAILED(HttpStatus.BAD_REQUEST, "파일 업로드에 실패했습니다."),
-    PRODUCT_NOT_FOUD(HttpStatus.NOT_FOUND, "일치하는 상품이 없습니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "일치하는 상품이 없습니다."),
     ALREADY_DELETED_PRODUCT(HttpStatus.NOT_FOUND, "이미 삭제된 상품입니다."),
     NOT_PRODUCT_ADMIN(HttpStatus.NOT_FOUND, "상품 게시자가 아닙니다."),
     REQUEST_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "요청 데이터의 형식을 확인해주세요.");

7280fbd7163654eb643dd0c8cff1189f51431631
haden <80521474+thdefn@users.noreply.github.com>
2024-08-27 15:05:28 +0900

    feat: add order processing cronJob script - return processing script - order cancellation script - received order shipping processing script

---
+++ cron-jobs/order-cancel-job.py
@@ -0,0 +1,29 @@
+import os
+import pymysql
+
+connection = None
+
+try:
+    db_url = os.getenv('DB_URL')
+    db_password = os.getenv('DB_PASSWORD')
+
+    connection = pymysql.connect(
+        host=db_url,
+        user="root",
+        password=db_password,
+        database='26cm-order')
+
+    with connection.cursor() as cursor:
+        cursor.execute("update orders set status = 'CANCEL' where status = 'PAYMENT_PENDING' and created_at <= NOW() - INTERVAL 2 DAY")
+        connection.commit()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+    exit(1)
+
+finally:
+    if connection:
+        connection.close()
+
+exit(0)
+
---
+++ cron-jobs/order-return-job.py
@@ -0,0 +1,75 @@
+import json
+import os
+import pymysql
+from kafka import KafkaProducer
+
+def get_kafka_producer():
+    return KafkaProducer(
+        bootstrap_servers=os.getenv('KAFKA_BROKERS'),
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+def main():
+    connection = None
+    producer = None
+
+    try:
+        db_url = os.getenv('DB_URL')
+        db_password = os.getenv('DB_PASSWORD')
+
+        producer = get_kafka_producer()
+
+        connection = pymysql.connect(
+            host=db_url,
+            user="root",
+            password=db_password,
+            database='26cm-order')
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT orderId, id, products FROM orders WHERE status = 'BEING_RETURNED'")
+
+            result = cursor.fetchall()
+
+            for row in result:
+                order_id = row[0]
+                id = row[1]
+                products = row[2]
+
+                json_dict = json.load(products)
+                product_quantity_map = {
+                    product_id: product_info['quantity']
+                    for product_id, product_info in json_dict.items()
+                }
+
+                order_cancelled_event = {
+                    'orderId': order_id,
+                    'productQuantity': product_quantity_map
+                }
+
+                producer.send('order-cancelled-events', value=order_cancelled_event)
+                cursor.execute(
+                    "UPDATE orders SET status = %s WHERE id = %s",
+                    ('RETURN_COMPLETED', id)
+                )
+            producer.flush()
+            connection.commit()
+
+
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        if connection:
+            connection.rollback()
+        exit(1)
+
+    finally:
+        if connection:
+            connection.close()
+        if producer:
+            producer.close()
+
+    exit(0)
+
+if __name__ == "__main__":
+    main()

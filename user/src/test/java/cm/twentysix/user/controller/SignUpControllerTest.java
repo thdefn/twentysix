68ac2b9f2d80c1bd105c557cc725e695bd7d3ff0
haden <80521474+thdefn@users.noreply.github.com>
2024-09-01 11:52:10 +0900

    chore: exclude feign-client component

--- user/src/test/java/cm/twentysix/user/controller/SignUpControllerTest.java
+++ user/src/test/java/cm/twentysix/user/controller/SignUpControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = SignUpController.class)
+@WebMvcTest(controllers = SignUpController.class, excludeFilters = @ComponentScan.Filter(FeignClient.class))
 @AutoConfigureMockMvc(addFilters = false)
 @AutoConfigureRestDocs
 class SignUpControllerTest {
